2c7d24b665eaf544e829a3444b60613647791700
CaioFML <caio.felipemotta@hotmail.com>
2019-06-04 23:07:25 -0300

    Definindo comportamento

--- src/bytebank/Conta.java
+++ src/bytebank/Conta.java
@@ -3,5 +3,31 @@ package bytebank;
 public class Conta {
 	double saldo;
 	int agencia, numero;
-	char titular;
+	String titular;
+	
+	public void deposita(double valor) {
+		this.saldo += valor;
+	}
+	
+	public boolean saca(double valor) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			System.out.println("Valor sacado: " + valor);
+			return true;
+		}else{
+			System.out.println("Você não tem saldo suficiente");
+			return false;
+		}
+	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		}else {
+			System.out.println("Não há dinheiro suficiente para transferir");
+			return false;
+		}
+	}
 }
--- src/bytebank/CriaConta.java
+++ src/bytebank/CriaConta.java
@@ -6,9 +6,14 @@ public class CriaConta {
 		Conta primeiraConta = new Conta();
 		primeiraConta.saldo = 200;
 		System.out.println(primeiraConta.saldo);
+		primeiraConta.deposita(500);
+		System.out.println(primeiraConta.saldo);
 		
 		Conta segundaConta = new Conta();
-		segundaConta.saldo = 300;
+		segundaConta.deposita(200);
+		segundaConta.saca(10);
+		
+		primeiraConta.transfere(200, segundaConta);
 		System.out.println(segundaConta.saldo);
 	}
 
