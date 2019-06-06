e39a03caa7ce67d83dc3300e060949cde8872194
CaioFML <caio.felipemotta@hotmail.com>
2019-06-05 22:50:05 -0300

    Ajustes

--- src/bytebank/TesteReferencias.java
+++ src/bytebank/TesteReferencias.java
@@ -4,8 +4,8 @@ public class TesteReferencias {
 	public static void main(String[] args) {
 		Conta primeiraConta = new Conta();
 		
-		primeiraConta.saldo = 300;
+		primeiraConta.setSaldo(300);
 		
-		System.out.println("saldo da primeira conta: " + primeiraConta.saldo);
+		System.out.println("saldo da primeira conta: " + primeiraConta.getSaldo());
 	}
 }
